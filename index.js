9954dd102cb00ecc31a05db65cd89e7ce9ef008b
Your Name <you@gmail.com>
2026-05-29 09:28:25 +0100

    Update cohort month to June 06

--- index.js
+++ index.js
@@ -167,7 +167,7 @@ app.post('/register', async (req, res) => {
     }
 
     const year = '26';
-    const month = '05';
+    const month = '06';
     const prefix = course + year + month;
 
     // Check if already registered
